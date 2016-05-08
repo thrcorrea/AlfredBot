0b9a706860e42a8cff4005be1fbb80209521156c
thrcorrea <thales.rcorrea@gmail.com>
2016-05-08 17:00:03 -0300

    Ajustes

--- bin/bot.js
+++ bin/bot.js
@@ -3,12 +3,10 @@
 var AlfredBot = require('../lib/alfredbot');
 
 var token = process.env.BOT_API_KEY;
-var dbPath = process.env.BOT_DB_PATH;
 var name = process.env.BOT_NAME;
 
 var alfredbot = new AlfredBot({
     token: token,
-    dbPath: dbPath,
     name: name
 });
 
